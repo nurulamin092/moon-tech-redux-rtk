1c7201332d72c9239b338330c35a26177d1bac76
nurulamin092 <mnurulamin092@gmial.com>
2022-12-18 09:28:18 +0600

    product slice added

--- src/app/store.js
+++ src/app/store.js
@@ -1,13 +1,15 @@
 import cartSlice from "../features/cart/cartSlice";
 import filterSlice from "../features/filter/filterSlice";
 import logger from "redux-logger"
+import productsSlice from "../features/products/productsSlice";
 
 const { configureStore } = require("@reduxjs/toolkit");
 
 const store = configureStore({
     reducer:{
         cart: cartSlice,
-        filter: filterSlice
+        filter: filterSlice,
+        product: productsSlice,
     },
     middleware:(getDefaultMiddleware)=>getDefaultMiddleware ().concat(logger)
 
